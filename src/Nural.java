d6db78338090ca38917739fcc1eafaedc0c26cac
bign8 <big.nate.w@gmail.com>
2012-04-22 01:29:24 -0600

    begining to teach myself neural networks - here goes

--- src/Nural.java
+++ src/Nural.java
@@ -1,21 +1,33 @@
 public class Nural {
 
-	//        							 SL   L   C   R  SR  bias
-	private static double[][] weights = { {-1, -2, -5, -2, -1, 9},  // motor
-		{ 1,  1,  5, -1, -1, 0},  // front steer
-		{ 0,  1, -5, -1,  0, 0} };// back steer
+	
+	private static double[][] weights = {
+	//     SL    L     C    R    SR   bias
+		{   0,  -.05, .2, -.05,   0,  0},  // motor
+		{ -10,  -25, -.5,   25,  10, 50},  // front steer
+		{ -10,   25,  .5,  -25, -10, 50} // back steer
+	};
 
 
+	private static double[][] training = {
+	//	 SL   L   C   R  SR  R1, R2, R3
+		{ 80, 80, 80, 80, 80, 16, 0, 0}, // regular driving
+		{}
+	};
+	
 	public static void main(String[] vars) {
-
+		run();
+	}
+	
+	public static void run() {
 		double sum0 = 0, sum1 = 0, sum2 = 0;
 		double[] toAdd = new double[6];
 		toAdd[5] = 1;
 		sum0 = 0; sum1 = 0; sum2 = 0;
 
-		toAdd[0] = 5.0;
-		toAdd[1] = 5.0;
-		toAdd[2] = 0.0;
+		toAdd[0] = 0.0;
+		toAdd[1] = 0.0;
+		toAdd[2] = 5.0;
 		toAdd[3] = 0.0;
 		toAdd[4] = 0.0;
 
@@ -26,15 +38,17 @@ public class Nural {
 
 		for (int i = 0; i < 6; i++) {
 			if ( toAdd[i] > 0 ) { // exclude poor input
-				sum0 += toAdd[i] * weights[0][i];
-				sum1 += toAdd[i] * weights[1][i];
-				sum2 += toAdd[i] * weights[2][i];
+				sum0 += 1/toAdd[i] * weights[0][i];
+				sum1 += 1/toAdd[i] * weights[1][i];
+				sum2 += 1/toAdd[i] * weights[2][i];
 			}
 		}
-
+		
+		sum0 = sum0 * 32 - 16; // allow negative speeds
+		sum1 *= 100; // adjust to turning distances
+		sum2 *= 100;
+		
 		// set down the smarts
 		System.out.println("| " + sum0 + " | " + sum1 + " | " + sum2 + " |");
-
-
 	}
 }
