7062a79237aa13d9fb1c443fa19e47e73f02e962
Jennings Anderson <jennings.anderson@gmail.com>
2012-04-27 16:10:41 -0600

    remote Works! Wahoo!
    Signed-off-by: Jennings Anderson <jennings.anderson@gmail.com>

--- src/RemoteTest.java
+++ src/RemoteTest.java
@@ -4,75 +4,53 @@ import com.ridgesoft.robotics.AnalogInput;
 
 public class RemoteTest {
 	public static void main(String args[]){
-		RemoteControl remote = new RemoteControl();
+		RemoteControl remote = new RemoteControl(true);
 		remote.run();
 	}
 }
 
-
-/* JENNINGS --- READ THIS PLZ
- * So - I physically plugged one pin into ground and the other into power and it did not fry the board
- * [Clairification] That is, ground to input pin 1 (read 0) and +5V to input pin 2 (read 1023) as expected
- * There is no possible way to produce more energy in that black box than it takes in, so I removed the
- * duplicate power in case that was the problem, either way, we should be golden
- * 
- * Also note that I fixed your code below, you should be ashamed of yourself btw
- * do not remove the "sum" line of the addNum function, I have a bit of magic for you with it.
- * 
- * If i'm not around when you get back, you should start working seeing how to stabilize the numbers still
- * Also, it would be good to bring the stuff over to desk or just commit your code so we can discuss it over on desk
- * 
- * Either way let me know...shoot me a txt on your plans
- * 						-N8
- */
-
-
 class RemoteControl{
 	private Display display;
 	private AnalogInput input1, input2, thumb; // remove thumb on decided size
+	private boolean debug;
+	private boolean remoteOn;
 
-	public RemoteControl(){
+	public RemoteControl(boolean debug){
 		display = IntelliBrain.getLcdDisplay();
 		input1 = IntelliBrain.getAnalogInput(1);
 		input2 = IntelliBrain.getAnalogInput(2);
 		thumb = IntelliBrain.getThumbWheel(); // remove thumb on decided size
+		this.debug = debug;
 	}
 
-	public void run(){	
-
-		//Averager port1 = new Averager();
-		//Averager port2 = new Averager();
-		//Maxer port1 = new Maxer();
-		//Maxer port2 = new Maxer();
-		//int sum1,sum2;
+	public void run(){
+		int r1, r2;
 		while(true){
-			display.print(0, "Port1: " + getRemote(input1));
-			//display.print(1, "Port2: " + getRemote(input2));	
-			
-//			port1.addNum(input1.sample());
-//			port2.addNum(input2.sample());
-//			
-//			port1.radix = port2.radix = thumb.sample() / 10; // remove line upon decided size
-//
-//			display.print(0, "Port1:" + port1.getAvg() + " rdx:" + port1.radix);
-//			display.print(1, "Port2:" + port2.getAvg() + " idx:"  + port2.index);
-//
-			try { Thread.sleep(100); } catch(Exception e) {};
-
+			r1 = input1.sample()/5 - 11;
+			r2 = input2.sample()/5 - 11;
+			if ((r1 < -5 )  || ( r2 < -5))
+				remoteOn = false;
+			else
+				remoteOn = true;
+			if (debug){
+				display.print(0, "Port1: "+r1 + " "+isOn());
+				display.print(1, "Port2: "+r2);
+			}
+			try { Thread.sleep(500); } catch(Exception e) {};
 		}
 	}
-	
-	public int getRemote(AnalogInput input){
-		int count=0;
-		while( input.sample() == 0 );
-		while( input.sample() > 0 ){
-			count++;
-		}
-		return count;
+	public boolean isOn(){
+		return remoteOn;
 	}
 }
 
 
+//Nate's Logic (It was good, but not applicable to this..)
+//port1.radix = port2.radix = thumb.sample() / 10; // remove line upon decided size
+//
+//display.print(0, "Port1:" + port1.getAvg() + " rdx:" + port1.radix);
+//display.print(1, "Port2:" + port2.getAvg() + " idx:"  + port2.index);
+/*
 class Averager{
 	public int nums[] = new int[50], index = 0, radix = 50, sum = 0; // radix will be converted to nums.length after testing
 
@@ -88,5 +66,5 @@ class Averager{
 		return sum / radix;	
 	}
 }
-
+*/
 
