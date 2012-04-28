b28c5dbd51b67c65c1274203ed00bd67cbd7cee1
bign8 <big.nate.w@gmail.com>
2012-04-28 00:43:57 -0600

    Self writing code ... I think so

--- src/Nural.java
+++ src/Nural.java
@@ -10,12 +10,12 @@ public class Nural {
 		
 		double[][] trainingData = {
 			//   W      L      C      R      E    V     F     B
-			{100.0, 100.0, 100.0, 100.0, 100.0, 3.0, 50.0, 50.0},
+			//{100.0, 100.0, 100.0, 100.0, 100.0, 3.0, 50.0, 50.0},
 			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
-			{},
-			{},
-			{},
-			{}
+			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
+			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
+			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
+			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5}
 		};
 		
 		double[][][] weights = {
@@ -45,13 +45,13 @@ public class Nural {
 				{ 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 }   // Hidden node 5
 			}*/
 		};
-		
+		/*
 		double[][] ofset = {
 			{ 0.0, 0.0, 0.0, 0.0, 0.0 }, // sigmoid ofsets 1
-			{ 0.0, 0.0, 0.0, 0.0, 0.0 }/*, // sigmoid ofsets 2
+			{ 0.0, 0.0, 0.0, 0.0, 0.0 }, // sigmoid ofsets 2
 			{ 0.0, 0.0, 0.0, 0.0, 0.0 }, // sigmoid ofsets 3
-			{ 0.0, 0.0, 0.0, 0.0, 0.0 }  // sigmoid ofsets 4*/
-		};
+			{ 0.0, 0.0, 0.0, 0.0, 0.0 }  // sigmoid ofsets 4
+		};//*/
 		
 		boolean[][] active = {
 			{ true, true, false, false, false, false }, // first one should always be true
@@ -63,101 +63,100 @@ public class Nural {
 		
 		double[][] outputs = new double[active.length][active[0].length]; // allows storage of past calcuations
 		
-		int i, j, k, l;
+		double[][] error = new double[weights.length][weights[0].length];
+		
+		int i, j, k, epoch, numberOfCycles = 10;
+		double learningRate = 1.0, curError, tempDiff;
 		
 		// -------------------------------------------------------
 		// |                     BEGIN EPOCH                     |
 		// -------------------------------------------------------
 		
-		outputs[0][0] = capper(trainingData[1][0] / 100.0, 1.0, 0.0);
-		outputs[0][1] = capper(trainingData[1][1] / 100.0, 1.0, 0.0);
-		outputs[0][2] = capper(trainingData[1][2] / 100.0, 1.0, 0.0);
-		outputs[0][3] = capper(trainingData[1][3] / 100.0, 1.0, 0.0);
-		outputs[0][4] = capper(trainingData[1][4] / 100.0, 1.0, 0.0); // for sensors
-		outputs[0][5] = 1.0;
-		outputs[1][5] = 1.0;
-		outputs[2][5] = 1.0;
-		//outputs[3][5] = 1.0;
-		//outputs[4][5] = 1.0;
+		for (epoch = 0; epoch < numberOfCycles; epoch++) {
 		
-		printArr(outputs[0], 50);
-		
-		// -------------------------------------------------------
-		// |                    QUERY NEURONS                    |
-		// -------------------------------------------------------
-		
-		// looping through the layers
-		for ( i = 0; i < weights.length; i++ ) {
+			outputs[0][0] = capper(trainingData[epoch % trainingData.length][0] / 100.0, 1.0, 0.0);
+			outputs[0][1] = capper(trainingData[epoch % trainingData.length][1] / 100.0, 1.0, 0.0);
+			outputs[0][2] = capper(trainingData[epoch % trainingData.length][2] / 100.0, 1.0, 0.0);
+			outputs[0][3] = capper(trainingData[epoch % trainingData.length][3] / 100.0, 1.0, 0.0);
+			outputs[0][4] = capper(trainingData[epoch % trainingData.length][4] / 100.0, 1.0, 0.0); // for sensors
+			outputs[0][5] = 1.0;
+			outputs[1][5] = 1.0;
+			outputs[2][5] = 1.0;
+			//outputs[3][5] = 1.0;
+			//outputs[4][5] = 1.0;
 			
-			// loop through rows of output
-			for ( j = 0; j < weights[0].length; j++ ) {
-				if ( active[i+1][j] ) {
-					for ( k = 0; k < weights[0][0].length; k++ ) if ( active[i][k] ) outputs[i+1][j] += outputs[i][k] * weights[i][j][k]; // sum rows
-					
-					outputs[i+1][j] = sigmoid( outputs[i+1][j] , ofset[i][j]); // perform threshold on sums
-				}
-			}
+			// -------------------------------------------------------
+			// |                    QUERY NEURONS                    |
+			// -------------------------------------------------------
 			
-			printArr(outputs[i+1], i); // debugging
-		}
-		
-		// scaling outputs of network - for actual use of neural network
-		//sums[0] = capper( sums[0] * 6 - 3 , -3.0 , 3.0 ); // [-3,3]
-		//sums[1] = capper( sums[1] * 100 , 0.0 , 100.0 );  // [0:100]
-		//sums[2] = capper( sums[2] * 100 , 0.0 , 100.0 );  // [0:100]
-		
-		System.out.println("[Backwards NOW]");
-		
-		// -------------------------------------------------------
-		// |                   BACKPROPAGATION                   |
-		// -------------------------------------------------------
-		
-		i = weights.length; // moving backward through layers
-		
-		// output error!
-		double[][] error = new double[weights.length][weights[0].length];
-		for ( j = 0; j < weights[0].length; j++ ) {
-			if ( active[i][j] )
-				error[i-1][j] = outputs[i][j] * ( 1 - outputs[i][j] ) * (trainingData[1][5+j] - outputs[i][j]);
-		}
-		printArr(error[i-1], 99);
-		
-		// new weights for output layer
-		for ( j = 0; j < weights[0].length; j++ ) {
-			if ( active[i][j] ) {
-				for ( k = 0; k < weights[0][0].length; k++ ) {
-					if (active[i-1][k]) {
-						weights[i-1][j][k] += outputs[i-1][k] * error[i-1][j];
+			// looping through the layers
+			for ( i = 0; i < weights.length; i++ ) {
+				
+				// loop through rows of output
+				for ( j = 0; j < weights[0].length; j++ ) {
+					if ( active[i+1][j] ) {
+						for ( k = 0; k < weights[0][0].length; k++ ) if ( active[i][k] ) outputs[i+1][j] += outputs[i][k] * weights[i][j][k]; // sum rows
+						
+						outputs[i+1][j] = sigmoid( outputs[i+1][j] , 0 /*ofset[i][j]*/); // perform threshold on sums
 					}
 				}
-				printArr(weights[i-1][j], 98);
 			}
-		}
-		
-		// change weights for hidden layers
-		for (i--; i > 0 ; i--) {
 			
-			//calculate errors in hidden layers
+			// scaling outputs of network - for actual use of neural network
+			//sums[0] = capper( sums[0] * 6 - 3 , -3.0 , 3.0 ); // [-3,3]
+			//sums[1] = capper( sums[1] * 100 , 0.0 , 100.0 );  // [0:100]
+			//sums[2] = capper( sums[2] * 100 , 0.0 , 100.0 );  // [0:100]
+			
+			// -------------------------------------------------------
+			// |                   BACKPROPAGATION                   |
+			// -------------------------------------------------------
+			
+			i = weights.length; // moving backward through layers
+			curError = 0.0;
+			
+			// output error!
+			error = new double[weights.length][weights[0].length]; // clear error to zeros
+			for ( j = 0; j < weights[0].length; j++ ) {
+				if ( active[i][j] ) {
+					tempDiff = (trainingData[epoch % trainingData.length][5+j] - outputs[i][j]);
+					error[i-1][j] = outputs[i][j] * ( 1 - outputs[i][j] ) * tempDiff;
+					curError += Math.pow(tempDiff, 2);
+				}
+			}
+			
+			// Display current calculated error
+			System.out.println("Epoch: " + (epoch+1) + " Error: " + curError);
+			
+			// new weights for output layer
 			for ( j = 0; j < weights[0].length; j++ ) {
 				if ( active[i][j] ) {
 					for ( k = 0; k < weights[0][0].length; k++ ) {
-						if (active[i+1][k]) {
-							error[i-1][j] += error[i][k] * weights[i][k][j];
+						if (active[i-1][k])
+							weights[i-1][j][k] += outputs[i-1][k] * error[i-1][j] * learningRate;
+					}
+				}
+			}
+			
+			// change weights for hidden layers
+			for (i--; i > 0 ; i--) {
+				
+				//calculate errors in hidden layers
+				for ( j = 0; j < weights[0].length; j++ ) {
+					if ( active[i][j] ) {
+						for ( k = 0; k < weights[0][0].length; k++ ) {
+							if (active[i+1][k])
+								error[i-1][j] += error[i][k] * weights[i][k][j];
 						}
+						error[i-1][j] *= ( 1 - outputs[i][j] ) * outputs[i][j];
 					}
-					error[i-1][j] *= ( 1 - outputs[i][j] ) * outputs[i][j];
 				}
-			}
-			printArr(error[i-1], 97);
-			
-			System.out.println("\n[Nate is working Here!]"); // TODO remove this line
-			
-			// calculate new layer weights
-			for ( j = 0; j < weights[0].length; j++ ) {
-				if ( active[i][j] ) {
-					for ( k = 0; k < weights[0][0].length; k++ ) {
-						if (active[i][k]) {
-							weights[i-1][j][k] += error[i-1][j] * outputs[i-1][k];
+				
+				// calculate new layer weights
+				for ( j = 0; j < weights[0].length; j++ ) {
+					if ( active[i][j] ) {
+						for ( k = 0; k < weights[0][0].length; k++ ) {
+							if (active[i][k])
+								weights[i-1][j][k] += error[i-1][j] * outputs[i-1][k] * learningRate;
 						}
 					}
 				}
@@ -165,32 +164,28 @@ public class Nural {
 		}
 		
 		// TODO Remove these debugging lines
-		System.out.println("\n[Weight Matrix]");
+		System.out.println("\n[Weight Matrix]\ndouble[][][] weights = {");
 		for (i = 0; i < weights.length; i++) {
+			System.out.print("\t{\n");
 			for (j = 0; j < weights[0].length; j++) {
-				for (k = 0; k < weights[0][0].length; k++)
-					System.out.print(weights[i][j][k] + " ");
+				System.out.print("\t\t{");
+				for (k = 0; k < weights[0][0].length; k++) {
+					System.out.print(weights[i][j][k] + "");
+					if (k != weights[0][0].length - 1) System.out.print(", ");
+				}
+				System.out.print("}");
+				if (j != weights[0].length - 1) System.out.print(",");
 				System.out.println();
 			}
+			System.out.print("\t}");
+			if (i != weights.length - 1) System.out.print(",");
 			System.out.println();
 		}
-		
-		System.out.println("[Errors Matrix]");
-		for (i = 0; i < error.length; i++) {
-			for (j = 0; j < error[0].length; j++)
-				System.out.print(error[i][j] + " ");
-			System.out.println();
-		}
+		System.out.println("};");
 	}
 	
 	public static double sigmoid( double x , double thresh ) {
 		return 1.0 / ( 1.0 + Math.exp(thresh-x) );
-	}
-	
-	public static void printArr( double [] x, int row ) {
-		System.out.print("Row " + row + " | ");
-		for (int i = 0; i < x.length; i++) System.out.print(x[i] + " | ");
-		System.out.println();
 	}
 	
 	public static double capper(double value, double max, double min) {
