d4e86e0a9810ffe2db8459adb68a327e1c1ddd5b
bign8 <big.nate.w@gmail.com>
2012-04-30 21:51:26 -0600

    It's official, its name is dumbo - neural networks is a failure

--- src/Nural.java
+++ src/Nural.java
@@ -5,20 +5,9 @@
  */
 public class Nural {
 	
-	public static void run() {
-		/*
-		double[][] trainingData = {
-			//   W      L      C      R      E    V     F     B
-			//{100.0, 100.0, 100.0, 100.0, 100.0, 3.0, 50.0, 50.0},
-			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
-			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
-			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
-			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5},
-			{35.0, 90.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5}
-		};//*/
+	public static double[][] trainingData = {
 		
 		//Straight on (slight approach to right) turn right
-		double[][] trainingData = {
 			{-1, 75, 69, 63, -1, 0, 50, 50},
 			{-1, 76, 69, 63, -1, 1, 50, 50},
 			{-1, 76, 68, 63, -1, 0, 50, 50},
@@ -647,36 +636,24 @@ public class Nural {
 			{48, 95, 103, 63, 49, 2, 50, 50},
 			{48, 92, 100, 60, 49, 1, 50, 50}
 		};
-		//*
-		double[][][] weights = {
-			{   //  W    L    C    R    E    B	
-				{ 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 },  // Hidden node 1 // Layer 1
-				{ 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 },  // Hidden node 2
-				{ 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 },  // Hidden node 3
-				{ 0.0, 0.0, 0.0, 1.0, 0.0, 0.0 },  // Hidden node 4
-				{ 0.0, 0.0, 0.0, 0.0, 1.0, 0.0 }   // Hidden node 5
-			},{
-				{ 1.0, 0.0, 0.0, 0.0, 0.0, 0.0 },  // Hidden node 1 // Layer 2
-				{ 0.0, 1.0, 0.0, 0.0, 0.0, 0.0 },  // Hidden node 2
-				{ 0.0, 0.0, 1.0, 0.0, 0.0, 0.0 },  // Hidden node 3
-				{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 },  // Hidden node 4
-				{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 }   // Hidden node 5
-			}
-		};//*/
+	
+	public static void run() {
 		
 		boolean[][] active = { // last column is bias!
-			{ true, true, true, true, true, true }, // first one should always be true
-			{ true, true, true, true, true, true },//*TODO: change bias back to true on this line after devl
-			{ true, true, true, false, false, false } // last one should only have three true
+			{ true, true, true, true, true, true, false, false }, // first one should always be true
+			{ true, true, true, true, true, true, true, true },
+			{ true, true, true, true, true, true, true, true },
+			{ true, true, true, true, true, true, true, true },
+			{ true, true, true, false, false, false, false, false } // last one should only have three true
 		};
 		
-		//double[][][] weights = new double[active.length-1][active[0].length][active[0].length-1];
+		double[][][] weights = new double[active.length-1][active[0].length][active[0].length-1];
 		weights = randomInit(weights, active);
 		
 		double[][] outputs = new double[active.length][active[0].length]; // allows storage of past calcuations
 		double[][] error = new double[weights.length][weights[0].length];
-		int i, j, k, epoch, numberOfCycles = 100;
-		double learningRate = 10.0d, curError, tempDiff;
+		int i, j, k, epoch, numberOfCycles = trainingData.length * 3;
+		double learningRate = 0.5d, curError, tempDiff;
 		
 		
 		for (i = 0; i < trainingData.length; i++) {
@@ -695,23 +672,14 @@ public class Nural {
 		// -------------------------------------------------------
 		
 		for (epoch = 0; epoch < numberOfCycles; epoch++) {
-			
-			/*
-			outputs[0][0] = (trainingData[epoch % trainingData.length][0] < 0) ? 100 : trainingData[epoch % trainingData.length][0] ;
-			outputs[0][1] = (trainingData[epoch % trainingData.length][1] < 0) ? 100 : trainingData[epoch % trainingData.length][1] ;
-			outputs[0][2] = (trainingData[epoch % trainingData.length][2] < 0) ? 100 : trainingData[epoch % trainingData.length][2] ;
-			outputs[0][3] = (trainingData[epoch % trainingData.length][3] < 0) ? 100 : trainingData[epoch % trainingData.length][3] ;
-			outputs[0][4] = (trainingData[epoch % trainingData.length][4] < 0) ? 100 : trainingData[epoch % trainingData.length][4] ;
-			
-			
-			outputs[0][0] = capper(outputs[0][0] / 100.0, 1.0, 0.0);
-			outputs[0][1] = capper(outputs[0][1] / 100.0, 1.0, 0.0);
-			outputs[0][2] = capper(outputs[0][2] / 100.0, 1.0, 0.0);
-			outputs[0][3] = capper(outputs[0][3] / 100.0, 1.0, 0.0);
-			outputs[0][4] = capper(outputs[0][4] / 100.0, 1.0, 0.0); // for sensors
-			//*/
 
-			for (i = 0; i < outputs.length; i++) outputs[i][5] = 1.0; // setup biases
+			outputs[0][0] = trainingData[epoch % trainingData.length][0];
+			outputs[0][1] = trainingData[epoch % trainingData.length][1];
+			outputs[0][2] = trainingData[epoch % trainingData.length][2];
+			outputs[0][3] = trainingData[epoch % trainingData.length][3];
+			outputs[0][4] = trainingData[epoch % trainingData.length][4];
+			
+			for (i = 0; i < outputs.length; i++) outputs[i][outputs[0].length-1] = 1.0; // setup biases
 			
 			// -------------------------------------------------------
 			// |                    QUERY NEURONS                    |
@@ -770,7 +738,7 @@ public class Nural {
 			for (i--; i > 0 ; i--) {
 				
 				//calculate errors in hidden layers
-				for ( j = 0; j < weights[0].length; j++ ) {
+				for ( j = 0; j < weights[0].length-1; j++ ) {
 					if ( active[i][j] ) {
 						for ( k = 0; k < weights[0][0].length; k++ ) { // fix possible error with -1 might want to verify
 							if (active[i+1][k]) {
