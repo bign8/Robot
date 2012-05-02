8a572e19e575e132aebbe70b97557fc87c020eda
bign8 <big.nate.w@gmail.com>
2012-05-01 21:14:11 -0600

    asdf

--- src/Debuggable.java
+++ src/Debuggable.java
@@ -1,5 +1,36 @@
+/*
+ * This interface allows a constant implementation of runnable thread
+ * A better implementation would be to have an abstract interface that all the classes could extend
+ * Unfortunantly, its a day away from the due date, so this is how its going to stay
+ */
 
 public interface Debuggable {
 	public void setRunning( boolean run );
 	public String[] toDebugString(String in[]);
+}
+
+// NOT IMPLEMENTED, but Briliant!
+abstract class RobotThread extends Thread implements Debuggable, Runnable { // other classes would extend this!
+	
+	private boolean running = true;
+	private int delay = 100;
+	public void setRunning(boolean run) { running = run; }
+	public void setDelay(int del) { delay = del; }
+
+	public void run() {
+		long time = System.currentTimeMillis();
+		while (true) {
+			
+			if (running) robotRun(); else robotStop();
+			
+			try {
+				time += delay;
+				Thread.sleep(time - System.currentTimeMillis());
+			} catch (Throwable t) { t.printStackTrace(); }
+		}
+	}
+	
+	abstract public void robotRun(); // single execution function when thread is running
+	abstract public void robotStop(); // single execution function when thread in stopped
+	abstract public String[] toDebugString(String[] in);
 }
--- src/Remote.java
+++ src/Remote.java
@@ -1,39 +1,66 @@
 import com.ridgesoft.intellibrain.IntelliBrain;
 import com.ridgesoft.robotics.AnalogInput;
+import com.ridgesoft.robotics.Smoother;
+
+/*
+ * Note: This class is designed for reading input from an "AR-1 2 channel reciever with reverse" from axial
+ * This device usally is directly connected to the servos, and thus produces a square wave
+ * The frequency of this square wave is to fast for the Intellibrain 2 processor to 'count' the lenght of a set or cleared bit
+ * This problem was fixed using a low pass filter that was turned the square wave into a mostly linear function we could sample
+ * 
+ * X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X
+ * X                                     X                                           X                                           X
+ * X    o-----/\/\/\/\------+-------o    X    V |   +---+             +---+          X    V |                                    X
+ * X          15 k Ohm      |            X      |   |   |             |   |          X      |                                    X
+ * X                      --+--          X      |   |   |     in      |   |          X      |               out                  X
+ * X   in                 10 uF    out   X      |   |   |             |   |          X      |                                    X
+ * X                      --+--          X      |   |   |             |   |          X      |--------------------------------    X
+ * X                        |            X      |---+   +-------------+   +------    X      |                                    X
+ * X    o-------------------+-------o    X      0--------------------------------    X      0--------------------------------    X
+ * X                                     X                                           X                                           X
+ * X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X X
+ */
 
 public class Remote implements Runnable, Debuggable {
-	private boolean running = true; // important
+	
+	// Port Reservations
+	private static final int AnologueInput1  = 2;
+	private static final int AnologueInput2  = 3;
+	
+	// Algorithm available variables
+	private boolean running = true, remoteOn = false;
 	private AnalogInput input1, input2;
-	private boolean remoteOn;
 	private int port1, port2;
-	private int sampleP1, sampleP2;
+	private Smoother sampleP1, sampleP2;
 
+	// Remote Constructor - optain analog input ports and initiate smoother
 	public Remote(){
-		input1 = IntelliBrain.getAnalogInput(2);
-		input2 = IntelliBrain.getAnalogInput(3);
+		input1 = IntelliBrain.getAnalogInput(AnologueInput1);
+		input2 = IntelliBrain.getAnalogInput(AnologueInput2);
+		
+		sampleP1 = new Smoother(0.2f, input1.sample());
+		sampleP2 = new Smoother(0.2f, input2.sample());
 	}
 
+	// Main running thread - recieve data from remote and smooth
 	public void run(){
+		int temp1, temp2;
 		long time = System.currentTimeMillis();
+		
 		while(true){
 			if (running) {
-				// TODO look into implementing smoother
-				// http://www.ridgesoft.com/robojde/2.0/docs/apidoc/com/ridgesoft/robotics/Smoother.html
 				// TODO look at getting wider data ranges
 				
-				sampleP1 = input1.sample();
-				sampleP2 = input2.sample();
+				temp1 = input1.sample();
+				temp2 = input2.sample();
 				
-				sampleP1 += input1.sample();
-				sampleP2 += input2.sample();
+				remoteOn = ( temp1 > 35 ) && ( temp2 > 35 ) && ( temp1 < 85 ) && ( temp2 < 85 );
 				
-				sampleP1 += input1.sample();
-				sampleP2 += input2.sample();
+				port1 = (int) ( sampleP1.smooth(temp1) / 5 - 12 );
+				port2 = (int) ( sampleP2.smooth(temp2) / 5 - 12 );
 				
-				port1 = sampleP1/15 - 12; // origionally divided by 5
-				port2 = sampleP2/15 - 12;
-				
-				remoteOn = ( port1 > -5 ) && ( port2 > -5 ) && ( port1 < 5 ) && ( port2 < 5 ); // added cap - absolutely necessary!
+			} else {
+				remoteOn = false;
 			}
 			
 			try {
@@ -43,14 +70,14 @@ public class Remote implements Runnable, Debuggable {
 		}
 	}
 	
+	// Debugging methods and accessors
+	public boolean isOn(){ return remoteOn; }
+	public int getPort1(){ return port1; }
+	public int getPort2(){ return port2; }
+	public void setRunning(boolean run) { running = run; }
 	public String[] toDebugString(String in[]) {
 		in[0] = "Remote Debug";
 		in[1] = ( remoteOn ? "On" : "Off" ) + " " + port1 + " " + port2;
 		return in;
 	}
-
-	public boolean isOn(){ return remoteOn; }
-	public int getPort1(){ return port1; }
-	public int getPort2(){ return port2; }
-	public void setRunning(boolean run) { running = run; }
 }
